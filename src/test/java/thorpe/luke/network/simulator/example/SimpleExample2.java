501292e455e572e79681a1dae064267b266e5efc
Luke  Thorpe <luke.thorpe18@imperial.ac.uk>
2021-11-17 19:26:28 +0000

    Lint

--- src/test/java/thorpe/luke/network/simulator/example/SimpleExample2.java
+++ src/test/java/thorpe/luke/network/simulator/example/SimpleExample2.java
@@ -1,5 +1,7 @@
 package thorpe.luke.network.simulator.example;
 
+import java.util.Random;
+import java.util.concurrent.*;
 import thorpe.luke.network.packet.NetworkCondition;
 import thorpe.luke.network.packet.NetworkCondition.InvalidNetworkConditionException;
 import thorpe.luke.network.packet.Packet;
@@ -7,9 +9,6 @@ import thorpe.luke.network.packet.PacketPipeline;
 import thorpe.luke.network.simulator.DistributedNetworkSimulator;
 import thorpe.luke.network.simulator.DistributedNetworkSimulator.InvalidSimulatorConfigurationException;
 import thorpe.luke.network.simulator.NodeManager;
-
-import java.util.Random;
-import java.util.concurrent.*;
 
 public class SimpleExample2 {
 
