9e8aae1307e6fc45eb4cc573c793af51c538c71c
thelukethorpe <luke.thorpe18@imperial.ac.uk>
2021-12-29 14:08:39 +0000

    Add test for NeutralPacketFilter class

---
+++ src/test/java/thorpe/luke/network/packet/NeutralPacketFilterTest.java
@@ -0,0 +1,24 @@
+package thorpe.luke.network.packet;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NeutralPacketFilterTest {
+
+  @Test
+  public void testEnqueueDequeueCycleIsFifo() {
+    NeutralPacketFilter neutralPacketFilter = new NeutralPacketFilter();
+    assertThat(neutralPacketFilter.tryDequeue()).isEmpty();
+    Packet packet1 = Packet.of("Packet #1");
+    Packet packet2 = Packet.of("Packet #2");
+    Packet packet3 = Packet.of("Packet #3");
+    neutralPacketFilter.enqueue(packet1);
+    neutralPacketFilter.enqueue(packet2);
+    neutralPacketFilter.enqueue(packet3);
+    assertThat(neutralPacketFilter.tryDequeue()).hasValue(packet1);
+    assertThat(neutralPacketFilter.tryDequeue()).hasValue(packet2);
+    assertThat(neutralPacketFilter.tryDequeue()).hasValue(packet3);
+    assertThat(neutralPacketFilter.tryDequeue()).isEmpty();
+  }
+}
