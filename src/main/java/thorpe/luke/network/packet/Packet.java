cbd26c57f9f6cf44bd407f457a0301a5a9ea79dd
thelukethorpe <luke.thorpe18@imperial.ac.uk>
2021-12-29 13:46:23 +0000

    Improve Packet class to contain common utility methods

--- src/main/java/thorpe/luke/network/packet/Packet.java
+++ src/main/java/thorpe/luke/network/packet/Packet.java
@@ -1,5 +1,7 @@
 package thorpe.luke.network.packet;
 
+import java.util.Arrays;
+
 public class Packet {
   private final byte[] data;
 
@@ -7,7 +9,32 @@ public class Packet {
     this.data = data;
   }
 
-  public byte[] getData() {
-    return data;
+  public static Packet of(String message) {
+    return new Packet(message.getBytes());
+  }
+
+  public String asString() {
+    return this.toString();
+  }
+
+  @Override
+  public int hashCode() {
+    return Arrays.hashCode(data);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
+    } else if (obj instanceof Packet) {
+      Packet that = (Packet) obj;
+      return Arrays.equals(this.data, that.data);
+    }
+    return false;
+  }
+
+  @Override
+  public String toString() {
+    return new String(data);
   }
 }
